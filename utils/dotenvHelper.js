95b1032b2ab6be4adc9cb348e790058e52aa49a0
Pedro Costa <125618804+pmdacosta@users.noreply.github.com>
2023-03-24 13:12:43 +0000

    added note about README file

--- utils/dotenvHelper.js
+++ utils/dotenvHelper.js
@@ -3,7 +3,8 @@ const fs = require('fs');
 function checkDotEnvExists() {
   const MESSAGE = "File .env was not found. \
 Please create a .env file before running the script. \
-You can use a sample.env as sample configuration.";
+You can use a sample.env as sample configuration. \
+See the README file for more information.";
 
   if (!fs.existsSync('.env')) {
     throw new Error(MESSAGE);
