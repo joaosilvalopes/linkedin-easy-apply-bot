44f7bccf2a8ff90a55817a6be6b792b08cae72fb
Christen <christen.bc@gmail.com>
2024-01-25 10:18:47 +0100

    enhance doc

--- sample_config.ts
+++ sample_config.ts
@@ -7,9 +7,9 @@ export default {
   KEYWORDS: "javascript",
   LOCATION: "Portugal",
   WORKPLACE: {
-    REMOTE: true,
-    ON_SITE: true,
-    HYBRID: false,
+    REMOTE: true, // whether you want remote jobs or not(true/false)
+    ON_SITE: true, // whether you want on site jobs or not(true/false)
+    HYBRID: true, // whether you want hybrid jobs or not(true/false)
   },
   JOB_TITLE: "(javascript|frontend|front-end|fullstack|full-stack|nodejs|node|js).*(developer|engineer)",
   JOB_DESCRIPTION: "^((?!(primeit))(.|[\n\r]))*$",
@@ -19,8 +19,8 @@ export default {
   PHONE: "912345678",
   CV_PATH: "",
   COVER_LETTER_PATH: "",
-  HOME_CITY: "Lisbon, Portugal",
-  YEARS_OF_EXPERIENCE: {
+  HOME_CITY: "Lisbon, Portugal", // an object with the languages as keys and your proficiency as values
+  YEARS_OF_EXPERIENCE: {  // an object with the skills as keys and the years of experience as values
     "angular": 5,
     "react.js": 6,
     ".net": 3,
@@ -49,14 +49,13 @@ export default {
     "spanish": "native",
     "french": "native"
   },
-  REQUIRES_VISA_SPONSORSHIP: false,
-  TEXT_FIELDS: { "salary": "60k" },
-  BOOLEANS: {
+  REQUIRES_VISA_SPONSORSHIP: false, // whether you require visa sponsorship or not(true/false)
+  TEXT_FIELDS: { "salary": "60k" }, // an object with the regexes to match input labels as keys and the input values as values (text value)
+  BOOLEANS: { // an object with the regexes to match input labels as keys and the input values as values (true/false value)
     "bachelhor|bacharelado": true,
     "authorized": true
   },
-  MULTIPLE_CHOICE_FIELDS: { "pronouns": "They/them" },
-
+  MULTIPLE_CHOICE_FIELDS: { "pronouns": "They/them" }, // an object with the regexes to match input labels as keys and the input values as values (option value)
   // OTHER SETTINGS
-  SINGLE_PAGE: false,
+  SINGLE_PAGE: false, // whether you want the applied job windows to close after applying(true/false)
 }
